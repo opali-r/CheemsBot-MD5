1e9a6fbf4a1e545a181f2b921576e7616763fd20
opali-r <120013244+opali-r@users.noreply.github.com>
2022-12-17 11:46:44 +0500

    Update config.js

--- config.js
+++ config.js
@@ -14,26 +14,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.vcardowner = ['923032112917'] //ur owner number
+global.ownername = "𓆩 𝐍𝐎𝐁𝐈𝐓𝐀 𓆪" //ur owner name
+global.ytname = "YT: 𓆩 𝐍𝐎𝐁𝐈𝐓𝐀 𓆪" //ur yt chanel name
+global.socialm = "GitHub: 𓆩 𝐍𝐎𝐁𝐈𝐓𝐀 𓆪" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213'] //ur number
-global.ownernomer = "916909137213" //ur number
-global.ownertag = '916909137213' //ur tag number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot V5' //ur bot name
-global.ownername = "🦄Dream Guy Xeon"
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD5' //script link
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.owner = ['923032112917'] //ur number
+global.ownernomer = "923032112917" //ur number
+global.ownertag = '923032112917' //ur tag number
+global.premium = ['923032112917'] //ur premium number
+global.botname = '𓆩 𝐍𝐎𝐁𝐈𝐓𝐀 𓆪 Bot V5' //ur bot name
+global.ownername = "𓆩 𝐍𝐎𝐁𝐈𝐓𝐀 𓆪"
+global.linkz = "https://wa.me/923032112917" //your theme url which will be displayed on whatsapp
+global.websitex = "https://wa.me/923032112917" //ur website to be displayed
+global.botscript = 'https://wa.me/923032112917' //script link
+global.themeemoji = "🇵🇰" //ur theme emoji
+global.packname = "made by" //ur sticker watermark packname
+global.author = "𓆩 𝐍𝐎𝐁𝐈𝐓𝐀 𓆪" //ur sticker watermark author
+global.wm = "𓆩 𝐍𝐎𝐁𝐈𝐓𝐀 𓆪 Bot Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
